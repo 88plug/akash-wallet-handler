962669fc68b9e811413f01f6b40bee9383d4131e
88plug <19512127+88plug@users.noreply.github.com>
2023-09-08 14:12:17 -0700

    Delete install-akash.sh

--- install-akash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-. variables
-apt-get update ; apt-get install -yqq curl unzip
-echo $AKASH_VERSION
-curl https://raw.githubusercontent.com/ovrclk/akash/master/godownloader.sh | sh -s -- v"$AKASH_VERSION"
